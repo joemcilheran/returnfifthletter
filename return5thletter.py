abc4b24602a1ea42e520267a4214aad4469abf33
joemcilheran <joe.mcilheran@gmail.com>
2016-04-12 17:20:20 -0500

    first commit

---
+++ return5thletter.py
@@ -0,0 +1,5 @@
+def returnfifthletter():  
+    InputString =input("Please type a word that is at least five letters long \n")
+    fifthLetter = InputString[4]
+    print(fifthLetter)
+returnfifthletter()
